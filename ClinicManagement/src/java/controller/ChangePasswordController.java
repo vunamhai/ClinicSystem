a5d61d825efe9ef18a308f7261f69de7cac4204f
Thao Nguyen <cuongkhainguyen1108@gmail.com>
2022-06-23 01:37:59 +0700

    fix

--- ClinicManagement/src/java/controller/ChangePasswordController.java
+++ ClinicManagement/src/java/controller/ChangePasswordController.java
@@ -13,6 +13,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -37,7 +38,7 @@ public class ChangePasswordController extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ChangePasswordController</title>");            
+            out.println("<title>Servlet ChangePasswordController</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet ChangePasswordController at " + request.getContextPath() + "</h1>");
@@ -58,7 +59,7 @@ public class ChangePasswordController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-               int id = Integer.parseInt(request.getParameter("id"));
+        int id = Integer.parseInt(request.getParameter("id"));
         request.setAttribute("id", id);
         request.getRequestDispatcher("jsp/changePassword.jsp").forward(request, response);
     }
@@ -74,49 +75,19 @@ public class ChangePasswordController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-      PrintWriter out = response.getWriter();
+        PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession();
         int id = Integer.parseInt(request.getParameter("id"));
         AccountDAOImpl aOImpl = new AccountDAOImpl();
         Accounts a = aOImpl.getAccountById(id);
         try {
-            String oldP = request.getParameter("oldPassword");
-            if (oldP.equals("") || oldP == null) {
-                throw new Exception();
-            }
-            String newP = request.getParameter("newPassword");
-            if (newP.equals("") || newP == null) {
-                throw new Exception();
-            }
-            String reNewP = request.getParameter("reNewPassword");
-            if (reNewP.equals("") || reNewP == null) {
-                throw new Exception();
-            }
-            if (oldP.equals(a.getPassword())!=true) {
-                out.println("<script type=\"text/javascript\">\n"
-                        + "alert('Old password not match!');\n"
-                        + "location= 'ChangePasswordController?id=" + id + "';\n"
-                        + "</script>");
-            }
-            if (newP.equals(reNewP) != true) {
-                out.println("<script type=\"text/javascript\">\n"
-                        + "alert('New password not match with confirm password!');\n"
-                        + "location= 'ChangePasswordController?id=" + id + "';\n"
-                        + "</script>");
-            }
-            if (newP.equals(reNewP) && oldP.equals(a.getPassword())) {
-                int check = aOImpl.updatePasswordById(id, newP);
-                if (check == 1) {
-                    out.println("<script type=\"text/javascript\">\n"
-                            + "alert('Change password successfully!');\n"
-                            + "location= 'http://localhost:8080/ClinicManagement/jsp/login.jsp';\n"
-                            + "</script>");
-                } else {
-                    out.println("<script type=\"text/javascript\">\n"
-                            + "alert('Change password unsuccessfully!');\n"
-                            + "location= 'ChangePasswordController?id=" + id + "';\n"
-                            + "</script>");
-                }
-            }
+            String oldPassword = request.getParameter("oldPassword").trim();
+            String newPassword = request.getParameter("newPassword").trim();
+            String reNewPassword = request.getParameter("reNewPassword").trim();
+            session.setAttribute("oldPassword", oldPassword);
+            session.setAttribute("newPassword", newPassword);
+            session.setAttribute("reNewPassword", reNewPassword);
+          
         } catch (Exception e) {
             out.println("<script type=\"text/javascript\">\n"
                     + "alert('Change password unsuccessfully!');\n"
