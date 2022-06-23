50c24b5378944c21ff68d7f5946842800e5d5423
Nguyễn Phương Uyên <105425565+uyennp01@users.noreply.github.com>
2022-06-23 10:37:55 +0700

    fix addService

--- ClinicManagement/src/java/controller/AddServiceController.java
+++ ClinicManagement/src/java/controller/AddServiceController.java
@@ -5,10 +5,13 @@
  */
 package controller;
 
+import dao.ServiceDAO;
 import dao.impl.ServiceDAOImpl;
 import entity.Service;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -32,18 +35,32 @@ public class AddServiceController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AddServiceController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AddServiceController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        request.setCharacterEncoding("UTF-8");
+            String[] ids = new String[10];
+        if (request.getParameter("list_doctors") != null) {
+            ids = request.getParameter("list_doctors").split("-");
         }
+        List<Integer> idList = new ArrayList<>();
+        
+        if (request.getParameter("list_doctors") != null) {
+            for (String i : ids) {
+                idList.add(Integer.parseInt(i));
+            }
+        }
+        
+        String serviceName = request.getParameter("serviceName");
+        String serviceBrief = request.getParameter("serviceDescription");
+        Service service = new Service();
+        service.setServiceName(serviceName);
+        service.setServiceDescription(serviceBrief);
+        ServiceDAO serviceDAO = new ServiceDAOImpl();
+        serviceDAO.addService(service);
+        
+        int key = serviceDAO.getIdInserted();
+        for (int k : idList) {
+        }
+        
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
