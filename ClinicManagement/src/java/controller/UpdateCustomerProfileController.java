7bbf121e4f17aa909b226a93469eecae85a3bec9
vunamhai <vunamhaimu@gmail.com>
2022-06-29 20:31:38 +0700

    Update UpdateCustomerProfileController.java

--- ClinicManagement/src/java/controller/UpdateCustomerProfileController.java
+++ ClinicManagement/src/java/controller/UpdateCustomerProfileController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller;
 
 import dao.UserDAO;
@@ -24,15 +20,7 @@ import javax.servlet.http.HttpSession;
 
 public class UpdateCustomerProfileController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, ParseException {
         response.setContentType("text/html;charset=UTF-8");
