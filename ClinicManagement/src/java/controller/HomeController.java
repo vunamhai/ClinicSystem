bfec4267693ba29cf6126abffe4d29df21809922
vunamhai <vunamhaimu@gmail.com>
2022-06-29 20:30:08 +0700

    Create HomeController.java

--- ClinicManagement/src/java/controller/HomeController.java
+++ ClinicManagement/src/java/controller/HomeController.java
@@ -9,7 +9,12 @@
  */
 package controller;
 
-import entity.Account;
+import dao.PostDAO;
+import dao.UserDAO;
+import dao.impl.PostDAOImpl;
+import dao.impl.UserDAOImpl;
+import entity.PostEntity;
+import entity.User;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.ServletException;
@@ -17,9 +22,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import dao.AccountDAO;
-import dao.impl.AccountDAOImpl;
-import entity.Account1;
 
 /**
  * * -This class uses function getUser in <code>dao.impl.UserDAOImpl</code> to
@@ -43,36 +45,20 @@ public class HomeController extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         HttpSession session = request.getSession();
-        Account1 user1 = (Account1) session.getAttribute("user");
-        if (user1 == null) {
-            String username = request.getParameter("username");
-            String password = request.getParameter("password");
-            AccountDAO userDAO = new AccountDAOImpl();
-            Account1 user = userDAO.login(username.trim(), password.trim());
-            if (user != null) {
-                session = request.getSession();
-                session.setAttribute("user", user);
-  
-              
-                if (user.getRole_id()== 1) {
-                    response.sendRedirect("ViewAllAccountController?page=1");
-                } else if (user.getRole_id() == 2) {
-                    request.getRequestDispatcher("ServiceManagementController").forward(request, response);
-                } else if (user.getRole_id()== 3) {
-                    response.sendRedirect("viewMyReservation");
-                } else {
-                    request.getRequestDispatcher("./jsp/home.jsp").forward(request, response);
-                }
-            } else {
-                request.setAttribute("message", "Tên đăng nhập hoặc mật khẩu không đúng!!!");
-                request.getRequestDispatcher("./jsp/login.jsp").forward(request, response);
-            }
-        } else {
+        User user = (User) session.getAttribute("user");
+        if (user != null) {
+            session = request.getSession();
+            session.setAttribute("user", user);
+            PostDAO postDAO = new PostDAOImpl();
+            List<PostEntity> posts = postDAO.getAllPost();
+            request.setAttribute("posts", posts);
             request.getRequestDispatcher("./jsp/home.jsp").forward(request, response);
+        } else {
+            request.getRequestDispatcher("./jsp/login.jsp").forward(request, response);
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
