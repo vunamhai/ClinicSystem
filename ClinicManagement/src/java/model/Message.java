c7847de6c1d8a497356db3938b1ed8a9fd2b8d3c
vunamhai <vunamhaimu@gmail.com>
2022-07-20 20:18:00 +0700

    Create Message.java

---
+++ ClinicManagement/src/java/model/Message.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+/**
+ *
+ * @author ADMIN
+ */
+public class Message {
+    String name;
+    String email;
+    String phone;
+    String note;
+
+    public Message(String name, String email, String phone, String note) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.note = note;
+    }
+
+    public Message() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
