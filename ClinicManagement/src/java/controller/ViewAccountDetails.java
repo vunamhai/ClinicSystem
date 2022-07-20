0a95a63597259460019e0966c841b958f808a4da
vunamhai <vunamhaimu@gmail.com>
2022-07-20 20:15:16 +0700

    Update ViewAccountDetails.java

--- ClinicManagement/src/java/controller/ViewAccountDetails.java
+++ ClinicManagement/src/java/controller/ViewAccountDetails.java
@@ -39,7 +39,7 @@ public class ViewAccountDetails extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-       
+        
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
